ae952f55bf17e579be870333161bd7d8e5e4a2a6
Christophe Biocca <christophe.biocca@gmail.com>
2019-12-24 20:09:09 -0500

    Add a property test that the generated regex matches every input string.

--- tests/property_tests.rs
+++ tests/property_tests.rs
@@ -1,4 +1,5 @@
 use grex::RegExpBuilder;
+use quickcheck::TestResult;
 use quickcheck_macros::quickcheck;
 use regex::Regex;
 use std::collections::HashSet;
@@ -14,4 +15,26 @@ fn produces_valid_regexes(input_strings: HashSet<String>) -> bool {
     )
     .build();
     Regex::new(&regexp).is_ok()
+}
+
+#[quickcheck]
+fn matches_each_input_string(input_strings: HashSet<String>) -> TestResult {
+    let regexp = RegExpBuilder::from(
+        input_strings
+            .iter()
+            .cloned()
+            .collect::<Vec<String>>()
+            .as_slice(),
+    )
+    .build();
+    let compiled_regex = Regex::new(&regexp);
+    if let Ok(compiled_regex) = compiled_regex {
+        TestResult::from_bool(
+            input_strings
+                .into_iter()
+                .all(|input| compiled_regex.is_match(&input)),
+        )
+    } else {
+        TestResult::discard()
+    }
 }
